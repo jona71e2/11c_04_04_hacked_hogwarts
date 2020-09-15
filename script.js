5093aa5d0787bf59db829e0d17ff62a0d18a3f3b
Jonas <jona71e2@stud.kea.dk>
2020-09-15 20:22:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,109 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+let students = [];
+
+const endpoint = "https://petlatkea.dk/2020/hogwarts/students.json";
+
+//The prototype for the student data
+const Students = {
+  firstName: "",
+  middleName: null,
+  lastName: "",
+  nickName: null,
+  gender: "",
+  image: "",
+  house: "",
+};
+
+function start() {
+  console.log("Program running");
+  loadJSON();
+}
+
+async function loadJSON() {
+  const response = await fetch(endpoint);
+  const jsonData = await response.json();
+
+  // when loaded, prepare data objects
+  prepareObjects(jsonData);
+}
+
+function prepareObjects(jsonData) {
+  console.log(jsonData);
+  students = jsonData.map(prepareObject);
+  console.log("Return value", students);
+
+  //TO DO - add buildList(); function
+  //buildList();
+}
+
+function prepareObject(jsonObject) {
+  console.log("PrepareObject", jsonObject);
+  const student = Object.create(Students);
+
+  //firstName
+  //Split the fullname-string from the json-file by every space.
+  const nameParts = jsonObject.fullname.trim().split(" ");
+  console.log("NameParts", nameParts.length);
+  //Set student firstName to be the first index of the nameParts-array
+  student.firstName = nameParts[0].capitalize();
+
+  //middleName
+  // if (nameParts.length > 2 && nameParts.length < 4) {
+  //   student.middleName = nameParts[1].capitalize();
+  // }
+  if (nameParts.length > 2) {
+    console.log("More than 2", nameParts);
+    const middleNames = [...nameParts].splice(1).shift();
+    console.log(":::MIDDLE NAMES", middleNames);
+
+    //student.middleName = middleNames.map((i) => i.capitalize()).join(" ");
+  }
+
+  //lastName
+  //Set the student lastName to be the last index of the namePart-array
+  student.lastName = nameParts[nameParts.length - 1].capitalize();
+  //Check if the last name includes a dash. If so, then create two substrings and capitalize both
+  if (student.lastName.includes("-")) {
+    student.lastName =
+      nameParts[1].substring(0, nameParts[1].indexOf("-") + 1).capitalize() +
+      student.lastName
+        .substring(student.lastName.indexOf("-") + 1)
+        .capitalize();
+    //console.log(student.lastName);
+  }
+
+  //nickName
+  if (jsonObject.fullname.includes(`"`)) {
+    console.log("KIG HER:::");
+    student.nickName = jsonObject.fullname.substring(
+      jsonObject.fullname.indexOf(`"`) + 1,
+      jsonObject.fullname.lastIndexOf(`"`)
+    );
+  }
+
+  student.gender = jsonObject.gender.trim().capitalize();
+
+  student.house = jsonObject.house.trim().capitalize();
+
+  student.image = `${student.lastName.toLowerCase()}_${student.firstName
+    .substring(0, 1)
+    .toLowerCase()}.png`;
+  if (student.image.includes("-")) {
+    student.image = `${student.lastName
+      .substring(student.lastName.indexOf("-") + 1)
+      .toLowerCase()}_${student.firstName.substring(0, 1).toLowerCase()}.png`;
+  }
+
+  return student;
+}
+
+String.prototype.capitalize = function () {
+  return this[0].toUpperCase() + this.substring(1).toLowerCase();
+};
+
+console.log("Hello");
+const myName = "frede";
+console.log(myName.capitalize());
